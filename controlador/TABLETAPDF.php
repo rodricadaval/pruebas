d39cef381795b62946e527fc6c791dbed0d7a899
Daniel <daniel.guerrero.94@gmail.com>
2017-07-04 16:17:30 -0300

    Cambio posicion label dni en pdfs

--- controlador/TABLETAPDF.php
+++ controlador/TABLETAPDF.php
@@ -107,7 +107,7 @@ $pdf->Cell(0, 0, $texto, 0, 1);
 if($area == "SESION"){
 	$y += 5;
 	$pdf->SetY($y);
-	$pdf->SetX(35);
+	$pdf->SetX(25);
 	$texto = HTML_ENTITIES_DECODE::text_to_pdf_decode("DNI: ");
 	$pdf->Cell(0, 0, $texto, 0, 1);
 }
